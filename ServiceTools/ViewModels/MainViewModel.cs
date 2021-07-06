ec8b88ee6e76f17285e76f6e65757d1b376485cb
evgeni <pandora207@live.com>
2021-07-06 21:05:26 +0300

    Add project files.

---
+++ ServiceTools/ViewModels/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+
+namespace ServiceTools.ViewModels
+{
+    class MainViewModel : ViewModelBase
+    {
+        private string titleWindow = "";
+
+        public MainViewModel() : base()
+        {
+            titleWindow = "Service Tools";
+        }
+
+        public string TitleWindow
+        {
+            get
+            {
+                return titleWindow;
+            }
+            set
+            {
+                value = titleWindow;
+                RaisePropertyChanged();
+            }
+        }
+    }
+}
