c8d0981adbfe90769aa04c8c2b8160c9772fdead
wastegate711 <evgeni.shvec@gmail.com>
2022-05-10 17:45:40 +0300

    Добавил очередь для отправки данных.

---
+++ Services/ServiceTools.Services.SerialPort/Interfaces/IMessageQueue.cs
@@ -0,0 +1,18 @@
+﻿namespace ServiceTools.Services.SerialPort.Interfaces;
+
+public interface IMessageQueue
+{
+    /// <summary>
+    /// Добавляет данные для отправки в конец очереди.
+    /// </summary>
+    /// <param name="data">Массив с данными которые нужно добавить в очередь.</param>
+    void AddMessageToQueue(byte[] data);
+
+    /// <summary>
+    /// Возвращает из очереди сообщений первое сообщение и удаляет его из очереди,
+    /// если очередь пуста вернет базовое сообщение
+    /// для Блока управления или Пульта.
+    /// </summary>
+    /// <returns>Сообщение для отправки его устройству.</returns>
+    byte[] GetMessageFromQueue();
+}
