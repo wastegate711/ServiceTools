2c7f6a5b89abc806eba100b7195fff45b30c0dbb
wastegate711 <evgeni.shvec@gmail.com>
2022-05-12 15:38:34 +0300

    базовые прием и передача данных готовы.

--- Services/ServiceTools.Services.SerialPort/Services/ReceivData.cs
+++ Services/ServiceTools.Services.SerialPort/Services/ReceivData.cs
@@ -10,13 +10,33 @@ namespace ServiceTools.Services.SerialPort.Services
 {
     public class ReceivData : IReceivData
     {
+        /*
+         * Формат сообщений
+         * [адрес ведущего 1 байт][адрес ведомого 1 байт][команда 1 байт][длина сообщения 1 байт][данные 0-251 байт][CRC16-2 байта]
+         */
         private bool accessFlag = false;
+        private const byte controlBlockAddr = 0x01; //адрес блока управления
+        private const byte pultBlockAddr = 0x02; //адрес пульта
+
+        public ReceivData()
+        {
+            
+        }
+        
         ///<inheritdoc/>
         public void ReadData(byte[] buf)
         {
             if (buf.CompareCrc16())
             {
-                accessFlag = true;
+                switch (buf[1])//определяем от какого блока пришли данные.
+                {
+                    case controlBlockAddr:
+
+                        break;
+                    case pultBlockAddr:
+
+                        break;
+                }
             }
         }
 
