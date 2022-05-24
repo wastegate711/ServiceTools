4560edd35a21170f1a47ce3c0947f40450ebe21a
wastegate711 <evgeni.shvec@gmail.com>
2022-05-24 14:40:04 +0300

    добавил кнопку и свойство с командой для нее.

--- Modules/ServiceTools.Modules.ControlBlock/ViewModels/ViewAViewModel.cs
+++ Modules/ServiceTools.Modules.ControlBlock/ViewModels/ViewAViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using ServiceTools.Services.SerialPort.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace ServiceTools.Modules.ControlBlock.ViewModels
 {
     public class ViewAViewModel : BindableBase
     {
-        public ViewAViewModel()
+        private readonly IMessageQueue _messageQueue;
+
+        public ViewAViewModel(IMessageQueue messageQueue)
         {
+            _messageQueue = messageQueue;
             DateTimeDevice = DateTime.Now.ToString(CultureInfo.CurrentCulture);
         }
         //Свойства
@@ -40,6 +46,32 @@ namespace ServiceTools.Modules.ControlBlock.ViewModels
         {
             get => _dateTimeDevice;
             set => SetProperty(ref _dateTimeDevice, value);
+        }
+
+        #endregion
+
+        #region Цвет кнопки Холодная вода
+
+        private SolidColorBrush _coolWaterBrush = Brushes.Red;
+        public SolidColorBrush CoolWaterBrush
+        {
+            get => _coolWaterBrush;
+            set => SetProperty(ref _coolWaterBrush, value);
+        }
+
+        #endregion
+        //Команды
+
+        #region Управление клапаном Холодная вода
+
+        private DelegateCommand _coolWaterCommand;
+        public DelegateCommand CoolWaterCommand =>
+            _coolWaterCommand ?? (_coolWaterCommand = new DelegateCommand(ExecuteCoolWater));
+
+        void ExecuteCoolWater()
+        {
+            byte[]
+            _messageQueue.AddMessageToQueue();
         }
 
         #endregion
