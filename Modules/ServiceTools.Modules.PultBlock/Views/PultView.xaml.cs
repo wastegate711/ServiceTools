5304cecb0bc62dd710a5af196ab83e5988fbc51d
wastegate711 <evgeni.shvec@gmail.com>
2022-06-15 11:49:02 +0300

    Переимновал классы view и viewmodel модуля pult.

--- Modules/ServiceTools.Modules.PultBlock/Views/PultView.xaml.cs
+++ Modules/ServiceTools.Modules.PultBlock/Views/PultView.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace ServiceTools.Modules.PultBlock.Views
 {
     /// <summary>
-    /// Interaction logic for ViewA.xaml
+    /// Interaction logic for PultView.xaml
     /// </summary>
-    public partial class ViewA : UserControl
+    public partial class PultView : UserControl
     {
-        public ViewA()
+        public PultView()
         {
             InitializeComponent();
         }
